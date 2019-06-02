6e89737ca2e373894b9cf26b910c39f1b9e31e28
ChillerDragon <ChillerDragon@gmail.com>
2019-06-02 17:31:19 +0200

    Support configs for macOS

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source "https://rubygems.org"
 
 gem "gosu"
+gem "os"
+gem "fileutils"
+
--- share/config.rb
+++ share/config.rb
@@ -1,13 +1,38 @@
 require 'json'
+require 'os'
+require 'fileutils'
 
 # chichilku3 config base used by client and server
 class Config
   attr_reader :data
 
   def initialize(console, file)
-    @file = file
+    chichilku3_dir = ""
+    if OS.linux?
+      chichilku3_dir = "#{ENV['HOME']}/.chichilku/chichilku3/"
+    elsif OS.mac?
+      chichilku3_dir = "#{ENV['HOME']}/Library/Application Support/chichilku/chichilku3/"
+    # elsif OS.windows?
+    #   chichilku3_dir = "%APPDATA%\\chichilku\\chichilku3\\"
+    else
+      puts "os not supported."
+      exit
+    end
+    puts "path: " + chichilku3_dir
+    FileUtils.mkdir_p chichilku3_dir
+    create_default_cfg(file, "#{chichilku3_dir}/#{file}")
+    @file = chichilku3_dir + file
     @console = console
     @data = load
+  end
+
+  def create_default_cfg(from, to)
+    return if File.file?(to)
+
+    tmp = JSON.parse(File.read(from))
+    File.open(to,"w") do |f|
+      f.write(tmp.to_json)
+    end
   end
 
   def sanitize_data(data)
