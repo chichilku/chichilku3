20232ac4800a9bcfebaaf324d8d8930ccf3583d9
ChillerDragon <ChillerDragon@gmail.com>
2020-04-13 12:42:02 +0200

    Fixed a whoopsie in the scoreboard UI

--- client/scoreboard.rb
+++ client/scoreboard.rb
@@ -8,14 +8,14 @@ def draw_scoreboard(win_size_x, win_size_y, players, font)
     slot_height = sizeY / MAX_CLIENTS
     text_scale = slot_height / 15
     # background
-    draw_rect(padX, padY, sizeX, sizeY, 0xaa000000)
+    draw_rect(padX, padY, sizeX, sizeY+3, 0xaa000000)
     # left border
-    draw_rect(padX, padY, 3, sizeY, 0xaa000000)
+    draw_rect(padX, padY, 3, sizeY+3, 0xaa000000)
     # right border
-    draw_rect(padX + sizeX - 3, padY, 3, sizeY, 0xaa000000)
+    draw_rect(padX + sizeX - 3, padY, 3, sizeY+3, 0xaa000000)
     (0..MAX_CLIENTS).each do |i|
-    # row borders
-    draw_rect(padX + 3, padY + (i * slot_height), sizeX - 6, 3, 0xaa000000)
+        # row borders
+        draw_rect(padX + 3, padY + (i * slot_height), sizeX - 6, 3, 0xaa000000)
     end
     players.each_with_index do | player, i |
         score_offset = text_scale * 10 * player.score.to_s.length
